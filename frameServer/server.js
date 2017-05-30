cb9014bcc8c04063732deec459ffaa06e3d68d62
Jamie Portsmouth <jamports@mac.com>
2017-05-30 21:47:20 +0100

    Update comments in server.js.

--- frameServer/server.js
+++ frameServer/server.js
@@ -1,7 +1,95 @@
 
+// Movie generation process:
 
-// Launch via (assuming node.js is installed):
-//  node server.js <output directory for images>
+// 1) Add the following logic in pre/postframeCallback, which will 
+// issue frames to the server once a sample theshold has been reached per-frame
+/*
+Scene.prototype.preframeCallback = function(snelly, gl)
+{
+	let renderer  = snelly.getRenderer();
+	let camera    = snelly.getCamera();
+	let controls  = snelly.getControls();
+	let materials = snelly.getMaterials();
+	let gui       = snelly.getGUI();
+
+	let FPS = 24.0;
+	let time = this.animFrame/FPS;
+	this.endFrame = 120.0; * FPS; // For example, for a 2 minute sequence
+
+	// animate camera (here a simple 'turntable' orbit about the original cam target)
+	let axis = camera.up;
+
+	if (this.animFrame > this.endFrame) this.animFrame = 0;
+	if (this.animFrame==0)
+	{
+		this.advanceFrame = false;
+		///
+		// Do any one-time initial setup of the scene state here
+		///
+	}
+
+	///
+	// Animate scene state according to current time value here
+	// (e.g. update scene, camera, materials or renderer parameters)
+	///
+
+	if (this.animFrame==0)
+	{
+		this.advanceFrame = false;
+	}
+
+	// Advance scene state to next anim frame, if we just exported a rendered frame
+	if (this.advanceFrame)
+	{	
+		gui.sync();
+		let no_recompile = true;
+		let reinit = false;
+		renderer.reset(no_recompile, reinit);
+		this.advanceFrame = false;
+	}
+}
+
+Scene.prototype.postframeCallback = function(snelly, gl)
+{
+	return;
+	let renderer  = snelly.getRenderer();
+	let targetSPP = 250.0;
+
+	// User code to post webGL framebuffer data to local server for processing
+	if (this.animFrame>=0 && renderer.spp>=targetSPP && this.animFrame<=this.endFrame)
+	{
+		console.log(this.animFrame);
+		let dataURI = gl.canvas.toDataURL();
+		var mimetype = dataURI.split(",")[0].split(':')[1].split(';')[0];
+		var byteString = atob(dataURI.split(',')[1]);
+		var u8a = new Uint8Array(byteString.length);
+		for (var i = 0; i < byteString.length; i++) 
+		{
+			u8a[i] = byteString.charCodeAt(i);
+		}
+		let blob = new Blob([u8a.buffer], { type: mimetype });
+		let r = new XMLHttpRequest();
+		r.open('POST', 'http://localhost:3999/' + this.animFrame, false);
+		r.send(blob);
+
+		this.advanceFrame = true;
+		this.animFrame++;
+	}
+}
+*/
+
+// 2) Launch this server via (assuming node.js is installed):
+//     node server.js <output directory for frame images>
+
+// 3) Launch the page to trigger rendering of frames.
+
+// 4) Convert resulting frames to a movie via e.g.:
+//    ffmpeg -r 24 -f image2 -i %05d.png -vb 20M video2.mp4
+
+// 5) Optionally, add an audio track via e.g. Avidemux, and export with with options:
+//  Video Output:  Mpeg4 AVC (x264)
+//  Audio Output:  AAC (Faac)
+// Output format:  MP4 Muxer
 
 
 function getCurrentDirectory() 
