1527be5747496697ebffccde2ac36f5378b35e71
Jamie Portsmouth <jamports@mac.com>
2020-12-23 18:30:37 +0000

    Update version.

--- js/snelly.js
+++ js/snelly.js
@@ -95,7 +95,7 @@ var Snelly = function(sceneObj)
 */
 Snelly.prototype.getVersion = function()
 {
-    return [1, 11, 7];
+    return [1, 12, 0];
 }
 
 Snelly.prototype.handleEvent = function(event)
